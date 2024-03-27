189aaf02d71e0138e62192cadcc61925fcd185ce
Tollski <TheTollski@gmail.com>
2024-03-27 01:42:24 -0600

    Added scripts for solo pvp games for insurgents. Removed profanity voice lines.

--- build.ps1
+++ build.ps1
@@ -3,8 +3,13 @@ New-Item -Path . -Name "ThirdPersonMod_vx" -ItemType "directory"
 Copy-Item -Path ".\cfg" -Destination ".\ThirdPersonMod_vx" -Recurse
 Copy-Item -Path ".\materials" -Destination ".\ThirdPersonMod_vx" -Recurse
 Copy-Item -Path ".\scripts" -Destination ".\ThirdPersonMod_vx" -Recurse
+Copy-Item -Path ".\sound" -Destination ".\ThirdPersonMod_vx"
+Copy-Item -Path ".\sound\hq" -Destination ".\ThirdPersonMod_vx\sound" -Recurse
+Copy-Item -Path ".\sound\player" -Destination ".\ThirdPersonMod_vx\sound" -Recurse
 Copy-Item -Path ".\readme.md" -Destination ".\ThirdPersonMod_vx"
 
 & "C:\Program Files (x86)\Steam\steamapps\common\insurgency2\bin\vpk.exe" ".\ThirdPersonMod_vx"
 
 Remove-Item -Path ".\ThirdPersonMod_vx" -Recurse
+
+Rename-Item -Path ".\thirdpersonmod_vx.vpk" -NewName "ThirdPersonMod_vx.vpk"
